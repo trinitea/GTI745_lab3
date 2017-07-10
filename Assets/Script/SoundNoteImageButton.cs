7d51195ed1bd98484d3702f2473749d0f49f4439
DESKTOP-ELGJ1VA\Level5-Chan <chan.jpeg@gmail.com>
2017-07-10 12:04:44 -0400

    UI Billboard for sound note.

---
+++ Assets/Script/SoundNoteImageButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundNoteImageButton : MonoBehaviour {
+    
+    private string SoundNoteOn;
+    private string SoundNoteOff;
+
+    public Color activeNoteColor;
+    public Color inactiveNoteColor;
+
+    private Color currentNoteColor;
+    private bool soundNoteState;
+
+    // Use this for initialization
+    void Start () {
+        soundNoteState = false;
+        GetComponent<Image>().color = inactiveNoteColor;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetNoteActiveColor() {
+
+        //Add note
+        if (soundNoteState == false) {
+            soundNoteState = true;
+            GetComponent<Image>().color = activeNoteColor;
+        }
+
+        //Remove note
+        else {
+            soundNoteState = false;
+            GetComponent<Image>().color = inactiveNoteColor;
+        }
+        //soundNoteState = !soundNoteState;
+    }
+}
